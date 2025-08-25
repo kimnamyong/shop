03bd476f394436b61454c8bf4c289f54bafb960c
GANGSA\LINE <knyatom@daum.net>
2025-08-25 16:43:01 +0900

    223232323232

--- shopmall/src/App.js
+++ shopmall/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import Data from './components/data';
+import { useState } from "react";
 function App() {
+  let [shoes, shoes변경] = useState(Data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Container className="background">
+        <h1>20% Season Off</h1>
+        <p>  This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.      </p>
+      </Container>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="100%"   />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content} & {shoes[0].price}원</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="100%"
+            />
+           <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content} & {shoes[1].price}원</p>
+          </div>
+
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="100%"
+            />
+           <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content} & {shoes[2].price}원</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
